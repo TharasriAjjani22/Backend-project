9ba99938ac07c7ba6c686104adaff653a93ed21d
TharasriAjjani22 <ajjanitharasri68@gmail.com>
2026-03-09 14:14:37 +0530

    Initial commit

---
+++ controllers/storeController.js
@@ -0,0 +1,9 @@
+
+
+import productModel from "../models/productModel.js";
+
+export const showProducts = async (req, res) => {
+  const products = await productModel.find();
+
+  res.render("store/products", { products });
+};
